37b8669399591c95df52cca249e11a1c25f2a961
seker212 <seker212@gmail.com>
2021-12-06 20:04:05 +0100

    Remade previous commit

--- LZ77/LZ77.py
+++ LZ77/LZ77.py
@@ -1,45 +1,70 @@
 import logging
+from typing import List, Optional, Tuple
 
 #TODO: Assert that sum of buffers' sizes is divisible by 8
 SEARCH_BUFFER_SIZE: int = 9
 LOOK_AHEAD_BUFFER_SIZE: int = 7
 
-def encrypt(string:str):
-    ptr_search: int = 0
+class EncryptBuffer:
+    def __init__(self, source: str) -> None:
+        self.ptr: int = 0
+        self.source = source #TODO: Change source to file
+        self.search_buffer: Optional[str] = None
+        self.look_ahead_buffer: Optional[str] = None
 
-    while ptr_search < len(string):
-        logging.debug(f'ptr_search: {ptr_search}')
+    def _set_search_buffer(self) -> None: #TODO: Change to reading from file
         search_buffer_begin = None
-        if ptr_search-SEARCH_BUFFER_SIZE < 0:
+        if self.ptr-SEARCH_BUFFER_SIZE < 0:
             search_buffer_begin = 0
         else:
-            search_buffer_begin = ptr_search-SEARCH_BUFFER_SIZE
-        logging.debug(f'search_buffer_begin: {search_buffer_begin}')
-        search_buffer: str = string[search_buffer_begin:ptr_search]
-        ahead_buffer: str = string[ptr_search:ptr_search+LOOK_AHEAD_BUFFER_SIZE]
-        logging.debug(f'Buffers: {search_buffer}\t{len(search_buffer)}\t\t{ahead_buffer}\t{len(ahead_buffer)}')
-        substring_len = None
-        prev_find_index = None
-        find_index = None
-        for i in range(len(ahead_buffer)+1):
-            if i > 0:
+            search_buffer_begin = self.ptr-SEARCH_BUFFER_SIZE
+        self.search_buffer = self.source[search_buffer_begin:self.ptr]
+        logging.debug(f'Search buffer set to: \'{self.search_buffer}\' (Len: {len(self.search_buffer)})')
+    
+    def _set_look_ahead_buffer(self) -> None: #TODO: Change to reading from file
+        self.look_ahead_buffer = self.source[self.ptr:self.ptr+LOOK_AHEAD_BUFFER_SIZE]
+        logging.debug(f'Look ahead buffer set to: \'{self.search_buffer}\' (Len: {len(self.search_buffer)})')
+
+    def _new_block(self) -> Tuple[Tuple[int, int, str], int]:
+        prev_find_index: Optional[int] = None
+        find_index: Optional[int] = None
+        
+        for substring_len in range(len(self.look_ahead_buffer)+1):
+            if substring_len > 0:
                 prev_find_index = find_index
-            logging.debug(f'Searching for substring: \'{ahead_buffer[:i]}\' ({i})')
-            find_index = search_buffer.find(ahead_buffer[:i])
-            if find_index == -1 or i == len(ahead_buffer):
+            logging.debug(f'Searching for substring: \'{self.look_ahead_buffer[:substring_len]}\' (substring_len = {substring_len})')
+            find_index = self.search_buffer.find(self.look_ahead_buffer[:substring_len])
+            if find_index == -1 or substring_len == len(self.look_ahead_buffer):
                 logging.debug(f'Substring not found.')
-                substring_len = i
-                if i == 1:
-                    logging.info(f'(0, 0, \'{ahead_buffer[0]}\')')
+                if substring_len == 1:
+                    logging.debug(f'Returning: (0, 0, \'{self.look_ahead_buffer[0]}\'), substring_len = {substring_len}')
+                    return ((0, 0, self.look_ahead_buffer[0]), substring_len)
                 else:
-                    logging.info(f'({len(search_buffer)-prev_find_index}, {substring_len-1}, \'{ahead_buffer[substring_len-1]}\')')
-                break
+                    logging.debug(f'Returning: ({len(self.search_buffer)-prev_find_index}, {substring_len-1}, \'{self.look_ahead_buffer[substring_len-1]}\')')
+                    return ((len(self.search_buffer)-prev_find_index, substring_len-1, self.look_ahead_buffer[substring_len-1]), substring_len)
             else:
                 logging.debug(f'Substring found. find_index= {find_index}')
-        print()
-        ptr_search += substring_len
+    
+    def encode_to_blocks(self) -> List[Tuple[int, int, str]]:
+        logging.info('Started encoding to blocks')
+        result_list: List[Tuple[int, int, str]] = []
+        while self.ptr < len(self.source): #TODO: Change source to file
+            logging.debug(f'ptr_search: {self.ptr}')
+            self._set_search_buffer()
+            self._set_look_ahead_buffer()
+            block, move_ptr = self._new_block()
+            self.ptr += move_ptr
+            result_list.append(block)
+            logging.debug(f'Block list: {result_list}')
+        logging.info('Encoded to blocks')
+        return result_list
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.NOTSET, format='%(levelname)s:\t%(message)s')
+    if (SEARCH_BUFFER_SIZE + LOOK_AHEAD_BUFFER_SIZE) % 8 != 0:
+        logging.error('Sum of buffers\' sizes isn\'t divisible by 8')
     string: str = 'abbcabcabbca'
-    encrypt(string)
+    result = EncryptBuffer(string).encode_to_blocks()
+    print('\n==============')
+    print(result)
+    print('==============')
