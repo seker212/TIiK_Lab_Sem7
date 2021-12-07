beb85847ee81887ce53cba011a622ac553ea7b12
seker212 <seker212@gmail.com>
2021-12-07 23:19:10 +0100

    Logging new line update

--- LZ77/LZ77.py
+++ LZ77/LZ77.py
@@ -6,7 +6,7 @@ from typing import Optional, Tuple
 
 SEARCH_BUFFER_BIT_SIZE: int = 4
 LOOK_AHEAD_BUFFER_BIT_SIZE: int = 4
-ENCODING_CHAR_BYTE_SIZE: int = 1
+ENCODING_CHAR_BYTE_SIZE: int = 2
 
 def number_to_bitstr(value: int, str_len: int):
     binstr: str = "{0:08b}".format(value)[-str_len:]
@@ -21,9 +21,9 @@ class Compressor:
         logging.debug('Compression source file opened')
         self.target = target #TODO: change target if file exists
         self.search_buffer: str = ''
-        logging.debug(f'Initiated search buffer: \'{self.search_buffer}\' (Len: {len(self.search_buffer)})')
+        logging.debug('Initiated search buffer: \'{0}\' (Len: {1})'.format(self.search_buffer.replace('\n', '\\n'), len(self.search_buffer)))
         self.look_ahead_buffer: str = self.source_file.read(pow(2, LOOK_AHEAD_BUFFER_BIT_SIZE)-1)
-        logging.debug(f'Initiated look ahead buffer: \'{self.look_ahead_buffer}\' (Len: {len(self.look_ahead_buffer)})')
+        logging.debug('Initiated look ahead buffer: \'{0}\' (Len: {1})'.format(self.look_ahead_buffer.replace('\n', '\\n'), len(self.look_ahead_buffer)))
 
     def __del__(self):
         self.source_file.close()
@@ -33,9 +33,9 @@ class Compressor:
         self.search_buffer += self.look_ahead_buffer[:lenght]
         if len(self.search_buffer) > pow(2, SEARCH_BUFFER_BIT_SIZE) - 1:
             self.search_buffer = self.search_buffer[len(self.search_buffer) - (pow(2, SEARCH_BUFFER_BIT_SIZE) - 1):]
-        logging.debug(f'Search buffer moved by {lenght}. New value: \'{self.search_buffer}\' (Len: {len(self.search_buffer)})')
+        logging.debug('Search buffer moved by {0}. New value: \'{1}\' (Len: {2})'.format(lenght, self.search_buffer.replace('\n', '\\n'), len(self.search_buffer)))
         self.look_ahead_buffer = self.look_ahead_buffer[lenght:] + self.source_file.read(lenght)
-        logging.debug(f'Look ahead buffer moved by {lenght}. New value: \'{self.look_ahead_buffer}\' (Len: {len(self.look_ahead_buffer)})')
+        logging.debug('Look ahead buffer moved by {0}. New value: \'{1}\' (Len: {2})'.format(lenght, self.look_ahead_buffer.replace('\n', '\\n'), len(self.look_ahead_buffer)))
 
     def _new_block(self) -> Tuple[Tuple[int, int, str], int]:
         prev_find_index: Optional[int] = None
@@ -44,15 +44,15 @@ class Compressor:
         for substring_len in range(len(self.look_ahead_buffer)+1):
             if substring_len > 0:
                 prev_find_index = find_index
-            logging.debug(f'Searching for substring: \'{self.look_ahead_buffer[:substring_len]}\' (substring_len = {substring_len})')
+            logging.debug('Searching for substring: \'{0}\' (substring_len = {1})'.format(self.look_ahead_buffer[:substring_len].replace('\n', '\\n'), substring_len))
             find_index = self.search_buffer.find(self.look_ahead_buffer[:substring_len])
             if find_index == -1 or substring_len == len(self.look_ahead_buffer):
-                logging.debug(f'Substring not found.')
+                logging.debug('Substring not found.')
                 if substring_len == 1:
-                    logging.debug(f'Returning: (0, 0, \'{self.look_ahead_buffer[0]}\'), substring_len = {substring_len}')
+                    logging.debug('Returning: (0, 0, \'{0}\'), substring_len = {1}'.format(self.look_ahead_buffer[0].replace('\n', '\\n'), substring_len))
                     return ((0, 0, self.look_ahead_buffer[0]), substring_len)
                 else:
-                    logging.debug(f'Returning: ({len(self.search_buffer)-prev_find_index}, {substring_len-1}, \'{self.look_ahead_buffer[substring_len-1]}\')')
+                    logging.debug('Returning: ({0}, {1}, \'{2}\')'.format(len(self.search_buffer)-prev_find_index, substring_len-1, self.look_ahead_buffer[substring_len-1].replace('\n', '\\n')))
                     return ((len(self.search_buffer)-prev_find_index, substring_len-1, self.look_ahead_buffer[substring_len-1]), substring_len)
             else:
                 logging.debug(f'Substring found. find_index= {find_index}')
@@ -72,7 +72,7 @@ class Compressor:
         offset_binstr = number_to_bitstr(block[1], LOOK_AHEAD_BUFFER_BIT_SIZE)
         char_binstr = number_to_bitstr(ord(block[2]), 8*ENCODING_CHAR_BYTE_SIZE)
         binstr = len_binstr + offset_binstr + char_binstr
-        logging.debug(f'Encoded ({block[0]}, {block[1]}, \'{block[2]}\') as \'{binstr}\'')
+        logging.debug('Encoded ({0}, {1}, \'{2}\') as \'{3}\''.format(block[0], block[1], block[2].replace('\n', '\\n'), binstr))
         return bytes([int(binstr[i:i+8], 2) for i in range(0, len(binstr), 8)])
 
 class Decompressor:
@@ -90,7 +90,7 @@ class Decompressor:
         self.search_buffer += value
         if len(self.search_buffer) > pow(2, SEARCH_BUFFER_BIT_SIZE) - 1:
             self.search_buffer = self.search_buffer[len(self.search_buffer) - (pow(2, SEARCH_BUFFER_BIT_SIZE) - 1):]
-        logging.debug(f'Moved search buffer. Appended value: \'{value}\'. New buffer value: \'{self.search_buffer}\' (Len: {len(self.search_buffer)})')
+        logging.debug('Moved search buffer. Appended value: \'{0}\'. New buffer value: \'{1}\' (Len: {len(self.search_buffer)})'.replace(value.replace('\n', '\\n'), self.search_buffer.replace('\n', '\\n')))
 
     def _read_block(self) -> Optional[Tuple[int, int, str]]:
         block_lo_bin: bytes = self.source_file.read(int((SEARCH_BUFFER_BIT_SIZE + LOOK_AHEAD_BUFFER_BIT_SIZE)/8))
@@ -102,7 +102,7 @@ class Decompressor:
         length: int = int(lenght_offset_binstr[:SEARCH_BUFFER_BIT_SIZE], 2)
         offset: int = int(lenght_offset_binstr[SEARCH_BUFFER_BIT_SIZE:], 2)
         char: str = chr(int.from_bytes(self.source_file.read(ENCODING_CHAR_BYTE_SIZE), byteorder='big', signed=False))
-        logging.debug(f'Returning new block: ({length}, {offset}, \'{char}\')')
+        logging.debug('Returning new block: ({0}, {1}, \'{2}\')'.format(length, offset, char.replace('\n', '\\n')))
         return (length, offset, char)
 
     def decompress(self) -> None:
@@ -115,17 +115,17 @@ class Decompressor:
             if block[0] == 0 and block[1] == 0:
                 with open(self.target, 'a') as target_file:
                     target_file.write(block[2]) 
-                    logging.debug(f'Appended to file: \'{block[2]}\' (Len: {len(block[2])})')
+                    logging.debug('Appended to file: \'{0}\' (Len: {1})'.format(block[2].replace('\n', '\\n'), len(block[2])))
                 self.search_buffer += block[2]
             else:
                 substring: str = self.search_buffer[len(self.search_buffer) - block[0]:len(self.search_buffer) - block[0] + block[1]]
                 with open(self.target, 'a') as target_file:
                     target_file.write(substring + block[2])
-                    logging.debug(f'Appended to file: \'{substring + block[2]}\' (Len: {len(block[2])})')
+                    logging.debug('Appended to file: \'{0}\' (Len: {1})'.format((substring + block[2]).replace('\n', '\\n'), len(block[2])))
                 self.search_buffer += substring + block[2]
             if len(self.search_buffer) > pow(2, SEARCH_BUFFER_BIT_SIZE) - 1:
                 self.search_buffer = self.search_buffer[len(self.search_buffer) - (pow(2, SEARCH_BUFFER_BIT_SIZE) - 1):]
-            logging.debug(f'Updated search buffer. New value: \'{self.search_buffer}\' (Len: {len(self.search_buffer)})')
+            logging.debug('Updated search buffer. New value: \'{0}\' (Len: {1})'.format(self.search_buffer.replace('\n', '\\n'), len(self.search_buffer)))
 
 
 if __name__ == '__main__':
